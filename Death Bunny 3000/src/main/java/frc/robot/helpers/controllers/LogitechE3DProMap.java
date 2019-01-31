3da77fc681df59308e5e032d635db494083f35ec
Noah Halstead <nhalstead00@gmail.com>
2019-01-30 19:10:33 -0500

    Updated the Controllers to be Final
    I forget to set the Ints to be Final, Basically making them a Constant.

--- Death Bunny 3000/src/main/java/frc/robot/helpers/controllers/LogitechE3DProMap.java
+++ Death Bunny 3000/src/main/java/frc/robot/helpers/controllers/LogitechE3DProMap.java
@@ -51,23 +51,23 @@ public class LogitechE3DProMap {
         }
     }
 
-    public static int BUTTON_TRIGGER = 1; // Button Value, A Button
-    public static int BUTTON_THUMB = 2; // Button Value, B Button
-    public static int BUTTON_3 = 3; // Button Value, Button 3
-    public static int BUTTON_4 = 4; // Button Value, Button 4
-    public static int BUTTON_5 = 5; // Button Value, Button 5
-    public static int BUTTON_6 = 6; // Button Value, Button 6
+    public static final int BUTTON_TRIGGER = 1; // Button Value, A Button
+    public static final int BUTTON_THUMB = 2; // Button Value, B Button
+    public static final int BUTTON_3 = 3; // Button Value, Button 3
+    public static final int BUTTON_4 = 4; // Button Value, Button 4
+    public static final int BUTTON_5 = 5; // Button Value, Button 5
+    public static final int BUTTON_6 = 6; // Button Value, Button 6
 
-    public static int BUTTON_7 = 7; // Button Value, Button 6
-    public static int BUTTON_8 = 8; // Button Value, Button 6
-    public static int BUTTON_9 = 9; // Button Value, Button 6
-    public static int BUTTON_10 = 10; // Button Value, Button 6
-    public static int BUTTON_11 = 11; // Button Value, Button 6
-    public static int BUTTON_12 = 12; // Button Value, Button 6
+    public static final int BUTTON_7 = 7; // Button Value, Button 6
+    public static final int BUTTON_8 = 8; // Button Value, Button 6
+    public static final int BUTTON_9 = 9; // Button Value, Button 6
+    public static final int BUTTON_10 = 10; // Button Value, Button 6
+    public static final int BUTTON_11 = 11; // Button Value, Button 6
+    public static final int BUTTON_12 = 12; // Button Value, Button 6
 
-    public static int AXIS_X = 0; // Axis Value, Left X Axis
-    public static int AXIS_Y = 1; // Axis Value, Left Y Axis
-    public static int AXIS_ROTATE = 2; // Axis Value, Right X Axis
-    public static int AXIS_SLIDER = 3; // Axis Value, Right Y Axis
+    public static final int AXIS_X = 0; // Axis Value, Left X Axis
+    public static final int AXIS_Y = 1; // Axis Value, Left Y Axis
+    public static final int AXIS_ROTATE = 2; // Axis Value, Right X Axis
+    public static final int AXIS_SLIDER = 3; // Axis Value, Right Y Axis
 
 }
--- Death Bunny 3000/src/main/java/frc/robot/helpers/controllers/LogitechMap_D.java
+++ Death Bunny 3000/src/main/java/frc/robot/helpers/controllers/LogitechMap_D.java
@@ -52,24 +52,24 @@ public class LogitechMap_D {
         }
     }
 
-    public static int BUTTON_A = 1; // Button Value, A Button
-    public static int BUTTON_B = 2; // Button Value, B Button
-    public static int BUTTON_X = 3; // Button Value, X Button
-    public static int BUTTON_Y = 4; // Button Value, Y Button
+    public static final int BUTTON_A = 1; // Button Value, A Button
+    public static final int BUTTON_B = 2; // Button Value, B Button
+    public static final int BUTTON_X = 3; // Button Value, X Button
+    public static final int BUTTON_Y = 4; // Button Value, Y Button
 
-    public static int BUTTON_LB = 5; // Button Value, LB Button
-    public static int BUTTON_RB = 6; // Button Value, RB Button
-    public static int BUTTON_LT = 7; // Button Value, LT Button
-    public static int BUTTON_RT = 8; // Button Value, RT Button
-    public static int BUTTON_BACK = 9; // Button Value, BACK Button
-    public static int BUTTON_START = 10; // Button Value, Start Button
+    public static final int BUTTON_LB = 5; // Button Value, LB Button
+    public static final int BUTTON_RB = 6; // Button Value, RB Button
+    public static final int BUTTON_LT = 7; // Button Value, LT Button
+    public static final int BUTTON_RT = 8; // Button Value, RT Button
+    public static final int BUTTON_BACK = 9; // Button Value, BACK Button
+    public static final int BUTTON_START = 10; // Button Value, Start Button
 
-    public static int BUTTON_LEFT = 11; // Button Value, Left Axis Button
-    public static int BUTTON_RIGHT = 12; // Button Value, Right Axis Button
+    public static final int BUTTON_LEFT = 11; // Button Value, Left Axis Button
+    public static final int BUTTON_RIGHT = 12; // Button Value, Right Axis Button
 
-    public static int AXIS_LEFT_X = 0; // Axis Value, Left X Axis
-    public static int AXIS_LEFT_Y = 1; // Axis Value, Left Y Axis
-    public static int AXIS_RIGHT_X = 4; // Axis Value, Right X Axis
-    public static int AXIS_RIGHT_Y = 5; // Axis Value, Right Y Axis
+    public static final int AXIS_LEFT_X = 0; // Axis Value, Left X Axis
+    public static final int AXIS_LEFT_Y = 1; // Axis Value, Left Y Axis
+    public static final int AXIS_RIGHT_X = 4; // Axis Value, Right X Axis
+    public static final int AXIS_RIGHT_Y = 5; // Axis Value, Right Y Axis
 
 }
