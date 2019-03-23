cbdeb713925edc16bc477ed6d634a7bd9f18ad25
Noah Halstead <nhalstead00@gmail.com>
2019-03-23 16:48:40 -0400

    Update LimelightFollow.java
    LimelightFollow.java:
     + Added Drive Control that was missing from the class.

--- Death Bunny 3000/src/main/java/frc/robot/commands/LimelightFollow.java
+++ Death Bunny 3000/src/main/java/frc/robot/commands/LimelightFollow.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Robot;
 import frc.robot.helpers.Limelight;
 
@@ -8,21 +9,38 @@ public class LimelightFollow extends Command {
 
     private double turnSensitivty = 50;
     private int cruiseVel = 400;
-    private int pipeline = 0;
+    private int pipeline;
+    private DifferentialDrive drive;
     private double angleError;
     private Limelight limelight;
 
-    public LimelightFollow(Limelight limelight, double timeout) {
-        this(timeout, 0);
+    /**
+     * 
+     * 
+     * @param drive Drive to Control
+     * @param limelight Limelight Object to read data from.
+     * @param pipeline Limelight Contorl to Find
+     */
+    public LimelightFollow(DifferentialDrive drive, Limelight limelight, int pipeline) {
+        this(drive, limelight, 5, pipeline);
     }
 
-    public LimelightFollow(double timeout, int pipeline){
+    /**
+     * 
+     * 
+     * @param drive Drive to Control
+     * @param limelight Limelight Object to read data from.
+     * @param pipeline Limelight Contorl to Find
+     * @param timeout The total time to allocate before it fails the execution.
+     */
+    public LimelightFollow(DifferentialDrive drive, Limelight limelight, int pipeline, int timeout){
         super(timeout);
         this.pipeline = pipeline;
+        this.drive = drive;
     }
 
     protected void initialize() {
-
+        System.out.println(this.getName() + ": Class has init the Execution.");
         limelight.setLed(Limelight.ledMode.kOn);
         limelight.setMode(Limelight.camMode.kVision);
         limelight.setPipeline(this.pipeline);
@@ -32,11 +50,17 @@ public class LimelightFollow extends Command {
     protected void execute() {
         angleError = limelight.getX() - 8;
         double turn = angleError * turnSensitivty;
-        Robot.m_drive.arcadeDrive(cruiseVel + turn, cruiseVel - turn);
+        this.drive.arcadeDrive(cruiseVel + turn, cruiseVel - turn);
     }
 
     protected boolean isFinished() {
         return Robot.ballLoaded.get() || isTimedOut();
+    }
+
+    protected void end(){
+        this.drive.stopMotor();
+        limelight.setLed(Limelight.ledMode.kOff);
+        System.out.println(this.getName() + ": Class has finished Execution.");
     }
 
 }
