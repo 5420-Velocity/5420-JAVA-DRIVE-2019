96efb77a4fb52964c13079b9318b9c242e6466b7
Asher Carney <ashman32@outlook.com>
2019-03-29 19:09:37 -0400

    Changed the limelight turn class to the example code in the docs

--- Death Bunny 3000/src/main/java/frc/robot/commands/Limelight_turn.java
+++ Death Bunny 3000/src/main/java/frc/robot/commands/Limelight_turn.java
@@ -3,29 +3,35 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.helpers.Limelight;
+/**
+ * Limelight_turn
+ * 
+ * This class will turn the robot to face a target using the limelight
+ * 
+ * @link http://docs.limelightvision.io/en/latest/cs_drive_to_goal_2019.html
+ */
 
 public class Limelight_turn extends Command {
+    final double STEER_K = 0.03;                    // how hard to turn toward the target
+    final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+    final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
+    final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
 
     private boolean isFinished = false;
     private DifferentialDrive driveSystem;
 
-    private double kp = -0.1f;
     private Limelight limelight;
 
-    private double leftSpeed;
-    private double rightSpeed;
-
-    private double minCommand = 0.05;
+    private double turnSpeed;
 
     public Limelight_turn(DifferentialDrive drive, Limelight limelight){
         this(drive, limelight, 0.2);
     }
 
-    public Limelight_turn(DifferentialDrive drive, Limelight limelight, double speed){
+    public Limelight_turn(DifferentialDrive drive, Limelight limelight, double turnSpeed){
 
         this.driveSystem = drive;
-        this.leftSpeed = speed;
-        this.rightSpeed = -speed;
+        this.turnSpeed = turnSpeed;
         this.limelight = limelight;
     }
 
@@ -35,22 +41,17 @@ public class Limelight_turn extends Command {
 
     protected void execute(){
 
-        double headingError = this.limelight.getX();
-        double steeringAdjust = 0;
-
-        if (Math.signum(headingError) == 1.0){
-            steeringAdjust = kp * headingError - minCommand;
+         // Start with proportional steering
+         double steer_cmd = this.limelight.getX() * STEER_K;
+         turnSpeed = steer_cmd;
+ 
+        if(this.limelight.hasTarget() == true) {
+            this.driveSystem.arcadeDrive(0, turnSpeed);
         }
-
-        else if (Math.signum(headingError) == 1.0){
-            steeringAdjust = kp * headingError + minCommand;
+        else{
+            this.driveSystem.arcadeDrive(0, 0);
+            this.isFinished = true;
         }
-
-        leftSpeed += steeringAdjust;
-        rightSpeed -= steeringAdjust;
-
-        this.driveSystem.tankDrive(leftSpeed, rightSpeed);
-
     }
 
     public boolean isFinished(){
--- Death Bunny 3000/src/main/java/frc/robot/commands/MotorDrive.java
+++ Death Bunny 3000/src/main/java/frc/robot/commands/MotorDrive.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PWMSpeedController;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.helpers.Locker;
