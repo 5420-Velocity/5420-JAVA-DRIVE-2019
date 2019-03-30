e0d4d7df666c5019a71fd5f39e84efbb8308afe2
Noah Halstead <nhalstead00@gmail.com>
2019-03-30 14:23:12 -0400

    Update Locker.java
    Added Locking Locking Function to Lock via a Class or PWMMotor Class Instance

--- Death Bunny 3000/src/main/java/frc/robot/helpers/Locker.java
+++ Death Bunny 3000/src/main/java/frc/robot/helpers/Locker.java
@@ -3,6 +3,8 @@ package frc.robot.helpers;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.wpi.first.wpilibj.PWMSpeedController;
+
 /**
  * Listener
  * Action Called when a new Log Event is Added.
@@ -23,12 +25,66 @@ public class Locker {
         return Locker.lockStatus.get(name.toUpperCase());
     }
 
+    /**
+     * Lock
+     * 
+     * Set a Key to be True, Create lock
+     * 
+     * @param name String Name
+     */
     public static void lock(String name){
         Locker.lockStatus.put(name.toUpperCase(), true);
     }
 
+    /**
+     * Lock via the Class Interface
+     * 
+     * @param t Class Instance
+     */
+    public static void lock(Class<?> t){
+        Locker.lockStatus.put("CLASS" +t.getCanonicalName(), true);
+    }
+
+    /**
+     * Lock
+     * 
+     * Set a Key to be True, Create lock from PWMSpeedController
+     * 
+     * @param SpeedCtrl Convert Speed Contorller to String Name
+     */
+    public static void lock(PWMSpeedController SpeedCtrl){
+        Locker.lockStatus.put("PWM" + SpeedCtrl.getName() + SpeedCtrl.getChannel(), true);
+    }
+
+    /**
+     * Unlock
+     * 
+     * Set a Key to be True, Create lock
+     * 
+     * @param name String Name
+     */
     public static void unlock(String name){
         Locker.lockStatus.put(name.toUpperCase(), false);
+    }
+
+    /**
+     * Unlock via the Class Interface
+     * 
+     * @param t Class Instance
+     */
+    public static void unlock(Class<?> t){
+        Locker.lockStatus.put("CLASS" +t.getCanonicalName(), false);
+    }
+
+    /**
+     * Unlock
+     * 
+     * Set a Key to be True, Create lock from PWMSpeedController
+     * 
+     * @param SpeedCtrl Convert Speed Contorller to String Name
+     */
+    public static void unlock(PWMSpeedController SpeedCtrl){
+        Locker.lockStatus.put("PWM" + SpeedCtrl.getName() + SpeedCtrl.getChannel(), false);
     }
 
 }
